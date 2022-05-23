5cfd7ccaa7deb55d815bec1456b8b3484917ff97
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-05-23 18:41:11 -0300

    Desafio 5

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -10,19 +10,68 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email } = this.props;
+    const { email, currencies } = this.props;
     return (
-      <header>
-        <h3 data-testid="email-field">{email}</h3>
-        <h3 data-testid="total-field"> 0 </h3>
-        <h3 data-testid="header-currency-field">BRL</h3>
-      </header>
+      <>
+        <header>
+          <h3 data-testid="email-field">{email}</h3>
+          <h3 data-testid="total-field"> 0 </h3>
+          <h3 data-testid="header-currency-field">BRL</h3>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor:
+            <input
+              data-testid="value-input"
+              name="value"
+              type="text"
+            />
+          </label>
+          <label htmlFor="description">
+            Descrição:
+            <input
+              data-testid="description-input"
+              name="description"
+              type="text"
+            />
+          </label>
+          <label htmlFor="Moeda">
+            Moeda:
+            <select name="Moeda" id="Moeda">
+              { currencies.map((curr) => (
+                <option key={ curr } value={ curr }>
+                  {curr}
+                </option>
+              )) }
+            </select>
+          </label>
+          <label htmlFor="payment-form">
+            Forma de Pagamento:
+            <select data-testid="method-input" name="payment-form">
+              <option value="Cash">Dinheiro</option>
+              <option value="Credit-Card">Cartão de crédito</option>
+              <option value="Debit-Card">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Categoria:
+            <select data-testid="tag-input" name="tag">
+              <option value="Feeding">Alimentação</option>
+              <option value="Leisure">Lazer</option>
+              <option value="Work">Trabalho</option>
+              <option value="Transportation">Transporte</option>
+              <option value="Health">Saúde</option>
+            </select>
+          </label>
+        </form>
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
   email: state.user.email,
+  currencies: state.wallet.currencies,
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -32,6 +81,7 @@ const mapDispatchToProps = (dispatch) => ({
 Wallet.propTypes = {
   email: PropTypes.string,
   fetchAPI: PropTypes.func,
+  currencies: PropTypes.arrayOf(PropTypes.string),
 }.isResquired;
 
 export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
