c9bddcc24be4cebcb38bf3de39a1884287890038
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-05-23 22:30:01 -0300

    Desafio 7

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -63,6 +63,19 @@ class Wallet extends React.Component {
               <option value="Health">Saúde</option>
             </select>
           </label>
+          <table>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </table>
         </form>
       </>
     );
